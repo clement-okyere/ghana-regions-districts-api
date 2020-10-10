ca26eb29adbe266036b650e481ce3e8dee6724af
okyere <clement.okyere@gmail.com>
2020-10-10 13:16:11 +0000

    added mongo db conn

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var mongoose = require("mongoose");
 const app = require("./src/loaders/server");
 bodyParser = require("body-parser");
 var regions = require("./src/routes/region");
@@ -7,6 +8,20 @@ const port = 3000;
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//Set up default mongoose connection
+var mongoDB = "mongodb://127.0.0.1/region_api";
+
+mongoose
+  .connect(mongoDB, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDb"))
+  .catch((err) => console.error("Could not connect to mongodb", err));
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.use("/regions", regions);
 
