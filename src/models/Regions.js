289640369f91bf8ce7218beae8528d02e14acd81
okyere <clement.okyere@gmail.com>
2020-10-10 13:16:33 +0000

    added bulk region insert endpoint

---
+++ src/models/Regions.js
@@ -0,0 +1,13 @@
+//Require Mongoose
+const mongoose = require("mongoose");
+
+const Region = mongoose.model(
+  "Region",
+  new mongoose.Schema({
+    name: { type: String, minlength: 2, unique: true },
+    capital: { type: String, minlength: 2 },
+    created_at: { type: Date, default: Date.now },
+  })
+);
+
+exports.Region = Region;
--- src/routes/region.js
+++ src/routes/region.js
@@ -1,8 +1,30 @@
+const { Region } = require("../models/Regions");
+var mongoose = require("mongoose");
 var express = require("express");
 var router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("All regions here");
+router.get("/", async (req, res) => {
+  const regions = await region.find();
+  res.send(regions);
+});
+
+//endpoint accepts an array of regions
+router.post("/", async (req, res) => {
+  try {
+    console.log("posted regions", req.body);
+    let regionsArray = [];
+    for (i = 0; i < req.body.length; i++) {
+      let region = new Region({
+        name: req.body[i].name,
+        capital: req.body[i].capital,
+      });
+      regionsArray.push(region);
+    }
+    const result = Region.insertMany(regionsArray);
+    res.status(200).send("regions inserted successfully", result);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 module.exports = router;
