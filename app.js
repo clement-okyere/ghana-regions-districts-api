465eec1995cb1e685ca9a5d464edfa16a4f3ac7e
okyere <clement.okyere@gmail.com>
2020-10-24 22:40:48 +0000

    exported app for test

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const express = require("express");
 const winston = require("winston");
 const app = express();
 
-const config = require("./src/config/config");
+const config = require("./src/config/config"); 
 const port = config.get("port");
 
-require("./src/startup/logging");
+require("./src/startup/logging")();
 require("./src/startup/route")(app);
 require("./src/startup/db")();
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   winston.info(
     `application listening on port ${port} at http://localhost:${port}`
   );
 });
+module.exports = server;
