bb6d92876c48025d26f633a8fb0cea28f9aa6e7a
okyere <clement.okyere@gmail.com>
2020-10-24 22:46:28 +0000

    removed unnecessary console logs

--- src/startup/route.js
+++ src/startup/route.js
@@ -1,11 +1,9 @@
-const express = require("express");
 const regions = require("../routes/region");
 const districts = require("../routes/district");
 const error = require("../middleware/error");
 bodyParser = require("body-parser");
 
 module.exports = function (app) {
-  console.log("inside routes");
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use("/regions", regions);
