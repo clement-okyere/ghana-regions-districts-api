173df35e17e70f9301ef0fdd3179fac6c4cfaef7
okyere <clement.okyere@gmail.com>
2020-10-24 22:42:01 +0000

    removed mongo authorization

--- src/startup/db.js
+++ src/startup/db.js
@@ -4,10 +4,13 @@ const config = require("../config/config");
 
 module.exports = function () {
   mongoose
-    .connect(
-      `mongodb://${config.get("db").host}/${config.get("db").database}`,
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    )
+    .connect(`mongodb://${config.get("db").host}:27017/${config.get("db").name}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => winston.info("Connected to MongoDB..."))
     .catch((err) => console.log(err));
 };
+
+
+
--- src/startup/logging.js
+++ src/startup/logging.js
@@ -1,19 +1,21 @@
 const winston = require("winston");
 require("winston-mongodb");
 require("express-async-errors");
+const config = require("../config/config");
 
 module.exports = function () {
-  winston.handleExceptions(
-    new winston.transports.File({ filename: "uncaughtExceptions.log" })
-  );
+  winston.exceptions.handle(new winston.transports.Console({ level: "error" }));
 
   process.on("unhandledRejection", (ex) => {
     throw ex;
   });
 
-  winston.add(winston.transports.File, { filename: "logfile.log" });
-  winston.add(winston.transports.MongoDB, {
-    db: `mongodb://${config.get("db").host}/${config.get("db").database}`,
-    level: "info",
-  });
+  
+  winston.add(new winston.transports.Console(), { level: "info" });
+  winston.add(
+    new winston.transports.MongoDB({
+      db: `mongodb://${config.get("db").host}:27017/${config.get("db").name}`,
+      level: "info",
+    })
+  );
 };
