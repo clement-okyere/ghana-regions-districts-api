5b1fcb1ca7c4e8931cb875ed97ad5b74b3e03a60
okyere <clement.okyere@gmail.com>
2021-01-02 11:46:11 +0000

    added swagger ui

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const app = express();
 
 const config = require("./src/config/config"); 
 const port = config.get("port");
+const swaggerUi = require('swagger-ui-express');
+let swaggerDocument = require('./swagger.json');
 
 require("./src/startup/logging")();
 require("./src/startup/route")(app);
 require("./src/startup/db")();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const server = app.listen(port, () => {
   winston.info(
